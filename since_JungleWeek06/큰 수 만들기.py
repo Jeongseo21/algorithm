4ea4054bd19aa6f4ee1979afc40709731d563107
jeongseo21 <jeongseo21@gmail.com>
2021-03-04 01:40:58 +0900

    .

---
+++ since_JungleWeek06/큰 수 만들기.py
@@ -0,0 +1,33 @@
+number = "0123546210"
+k = 8
+
+nums = list(map(int, number))
+surviving = []
+cnt = 0
+
+while(cnt < k and len(nums) > 1):
+    temp = nums.pop(0)
+
+    if temp >= nums[0]:
+        surviving.append(temp)
+    else:
+        cnt += 1
+        while(surviving != [] and cnt < k):
+            min_num = min(surviving)
+            if min_num > nums[0]:
+                break
+            surviving.remove(min_num)
+            cnt += 1
+
+surviving.extend(nums)
+if (k - cnt > 0):
+    while cnt != k:
+        min_num = min(surviving)
+        surviving.remove(min_num)
+        cnt += 1
+new = list(map(str, surviving))
+print("".join(new))
+
+
+
+
